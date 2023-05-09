3fcba04207e75ace82c0a1a25c81eae13ed1983f
Edwin <edgon85@gmail.com>
2023-05-09 16:00:35 -0600

    creando conexion a mongodb con mongoose y creando esquemas y models

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,16 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  // id: string // Mongoose me lo da
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
